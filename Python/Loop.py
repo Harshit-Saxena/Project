51616e51702193753ce46a6c231f21f4664d0160
Pulkit-s21 <77536409+Pulkit-s21@users.noreply.github.com>
2021-07-09 11:51:26 +0530

    New

--- Python/Loop.py
+++ Python/Loop.py
@@ -3,7 +3,7 @@ while i <=10:  # ! : is very imp to go into the while loop
      print(i, end = ' ') # Here we chngd the final value of \n which means nxt line but we chngd it to space
      i += 1
 
-print('\n') # * Here I added this to change the line after 1 loop is finished and then print 2nd loop in other  
+print() # * Here I added this to change the line after 1 loop is finished and then print 2nd loop in other  
 
 
 i=10
---
+++ Python/NestedWhileLoop.py
@@ -0,0 +1,45 @@
+# For star like shape for string
+
+name='CHITKARA'
+for i in range(len(name)): #Rows
+     for j in range(i + 1): #Columns
+          print(name[j],end = " ") 
+     print()
+
+print('\n');
+
+
+# For single character in a row
+
+for i in (name):
+     print(i)
+
+print('\n')
+
+# 3rd value is the increment value in for loop 
+for i in range(1,51,3):
+     print(i,end = " ")
+
+print('\n')
+
+# While loop for star like shape for numbers
+
+i=1
+while i<=5:
+     j=1
+     while j<=i:
+          print(j,end= " ")
+          j+=1
+     i+=1
+     print()
+
+print('\n')
+
+# For loop for the star like shape of numbers
+
+for i in range(1, 6):
+     for j in range(1, i + 1):
+          print(j,end= " ")
+     print()
+
+print('\n')
