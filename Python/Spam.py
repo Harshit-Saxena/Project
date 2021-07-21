3f457f700059122e11a0e059af63fccd23d302d3
Pulkit-s21 <77536409+Pulkit-s21@users.noreply.github.com>
2021-07-21 16:19:22 +0530

    New

--- Python/Spam.py
+++ Python/Spam.py
@@ -1,6 +1,6 @@
 import pyautogui, time
 time.sleep(5)
-file = open("C:\VScode\JavaScript\Python\Text\geemovie.txt", 'r')
+file = open("C:\VScode\JavaScript\Python\Text\geemovie.txt", 'r')  # * r is for read the file
 for word in file:
      pyautogui.typewrite(word)
      pyautogui.press("enter")
