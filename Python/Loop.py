761ef6f6d3fbf8b487a0dd42df0e1dacf08149fc
Pulkit-s21 <77536409+Pulkit-s21@users.noreply.github.com>
2021-07-22 11:06:40 +0530

    New

--- Python/Loop.py
+++ Python/Loop.py
@@ -24,4 +24,12 @@ prices = [10,20,30,40,50]
 total = 0
 for i in prices:
      total +=i
-print(f"Total of the items in the list: Rs {total}")
+print(f"Total of the items in the list: Rs {total}")    
+               
+
+# While loop for verticle
+list=[1,2,3,4,'Name','Place']
+i=0
+while i< len(list):
+     print(list[i])
+     i+=1
